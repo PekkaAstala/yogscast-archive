2312fa3af84575b927c3b84ecc26c2117633b214
Pekka Astala <pekka.astala@sysart.fi>
2016-12-11 16:10:24 +0200

    Added base for youtube searching lambda

---
+++ youtube-search.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let https = require('https')
+
+exports.handler = (event, context, callback) => {
+
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? ':(' : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+
+    let youtubeReqParams = {
+        host: "content.googleapis.com",
+        path: "/youtube/v3/search" +
+            "?q=terraria" +
+            "&safeSearch=none" +
+            "&order=relevance" +
+            "&channelId=UCH-_hzb2ILSCo9ftVSnrCIQ" +
+            "&part=snippet" +
+            "&key=" + process.env.YOUTUBE_API_KEY
+    }
+
+    let resultReducer = element => {
+        return {
+            "title": element.snippet.title,
+            "url": "https://www.youtube.com/watch?v=" + element.id.videoId
+        }
+    }
+
+    let youtubeCallback = function(response) {
+      let str = ''
+
+      response.on('data', function (chunk) {
+        str += chunk
+      });
+
+      response.on('end', function () {
+          done(null, JSON.parse(str).items.map(resultReducer))
+      })
+    }
+
+    https.request(youtubeReqParams, youtubeCallback).end()
+}
